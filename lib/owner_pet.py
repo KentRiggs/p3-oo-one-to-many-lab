bc0bba04a98cb6b79fe307c3747f6798fa7b9fc4
kent riggs <k2r3@proton.me>
2024-02-28 20:31:29 -0700

    added code to satisfy tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Pet is not a valid pet type")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = None
+        Pet.all.append(self)
+        if owner and isinstance(owner, Owner):
+            owner.add_pet(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+    
+    def pets(self):
+        return self._pets
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Pet is not a valid pet type")
+        pet.owner = self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
